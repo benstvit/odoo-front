29fe4cbe8000ba1ff3f0468d6e9555f35d227f79
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-11-03 14:31:33 +0100

    work in progress

--- src/main.js
+++ src/main.js
@@ -1,23 +1,14 @@
 import Vue from 'vue';
-import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './index.css';
-import { securedAxiosInstance, plainAxiosInstance } from './backend/axios';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
-Vue.use(VueAxios, {
-  secured: securedAxiosInstance,
-  plain: plainAxiosInstance,
-});
-
 new Vue({
-  plainAxiosInstance,
   router,
-  securedAxiosInstance,
   store,
   vuetify,
   render: (h) => h(App),
