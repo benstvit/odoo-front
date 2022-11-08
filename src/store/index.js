887164bb74e5353b9fb2a968be7ba6ac6b025585
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-11-08 16:22:04 +0100

    set backend url

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex';
 import axios from 'axios';
 
 const axiosConfig = {
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://odoo-scrapper-api.herokuapp.com/api/v1',
   timeout: 30000,
 };
 
